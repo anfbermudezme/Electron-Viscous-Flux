9f6d7e9dc471cb1fdebd79b75157b875c8284831
anfbermudezme <andres.bermudez@edunext.co>
2023-11-08 23:07:55 +0100

    fix: correct diffusive simulation.

--- Resources/simulationWithDamping.m
+++ Resources/simulationWithDamping.m
@@ -1,23 +1,32 @@
-function simulationWithDamping(dampingCoefficient)
-    number_of_elements = 50;  % number of elements
-    L = 1;  % length of domain
-    nu = 0.01;  % viscosity
-    f = 1.0;  % force
-    u0 = 0;
-    uL = 0;
-    dx = L / number_of_elements;  % size of each element
+% Parameters
+number_of_elements = 50;  % number of elements
+L = 1;  % length of domain
+nu = 0.01;  % viscosity
+f = 1.0;  % force
+u0 = 0;
+uL = 0;
+dx = L / number_of_elements;  % size of each element
+beta_values = [0.05, 0.1, 0.15]; % Array of beta values
 
-    % Initialize matrices
-    K = zeros(number_of_elements+1, number_of_elements+1);
-    F = zeros(number_of_elements+1, 1);
+% Create a figure
+figure;
+hold on;
 
+for b = 1:length(beta_values)
+    beta = beta_values(b);
+
+    K = zeros(number_of_elements + 1, number_of_elements + 1);
+    F = zeros(number_of_elements + 1, 1);
+
+    % Assembly
     for i = 1:number_of_elements
-        k_local = [
-            nu/dx - 0.5*(u0 + uL)/2 + dampingCoefficient, -nu/dx + 0.5*(u0 + uL)/2 - dampingCoefficient;
-            -nu/dx - 0.5*(u0 + uL)/2 - dampingCoefficient, nu/dx + 0.5*(u0 + uL)/2 + dampingCoefficient;
-        ];
-        f_local = [f*dx/2; f*dx/2];
-        
+        k_local = [nu/dx - 0.5*(u0 + uL)/2 + beta, -nu/dx + 0.5*(u0 + uL)/2;
+                   -nu/dx - 0.5*(u0 + uL)/2, nu/dx + 0.5*(u0 + uL)/2 + beta];
+
+        % Local load vector
+        f_local = [f * dx / 2; f * dx / 2];
+
+        % Assembly to global matrix
         K(i:i+1, i:i+1) = K(i:i+1, i:i+1) + k_local;
         F(i:i+1) = F(i:i+1) + f_local;
     end
@@ -33,11 +42,16 @@ function simulationWithDamping(dampingCoefficient)
     K(end, end) = 1;
     F(end) = uL;
 
-    % Solve
-    u = K\F;
-    x = linspace(0, L, number_of_elements+1);
+    u = K \ F;
+    x = linspace(0, L, number_of_elements + 1);
 
-    % Plot
-    plot(x, u, '-o', 'LineWidth', 2, 'MarkerSize', 6);
-    hold on;
+    plot(x, u, 'o-', 'LineWidth', 2, 'DisplayName', ['\beta = ' num2str(beta)]);
 end
+
+% Set plot details
+xlabel('Position (x)');
+ylabel('Velocity (u)');
+title('1D Navier-Stokes Velocity-Position for Different \beta Values');
+grid on;
+legend;
+hold off;
